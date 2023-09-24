f810271e5bb3b02975af5c4a938e4f2449b61b56
christina <headsteins@gmail.com>
2023-09-24 11:53:12 +0800

    fix incorrect style way

--- src/components/Micro.tsx
+++ src/components/Micro.tsx
@@ -4,7 +4,7 @@ import { Image } from "expo-image";
 
 export default function Micro() {
   return (
-    <TouchableOpacity style={{}} className="absolute w-full z-30 flex justify-center   flex-row   items-center  rounded-full bottom-24">
+    <TouchableOpacity className="absolute w-full z-30 flex justify-center   flex-row   items-center  rounded-full bottom-24">
       <View className="bg-[#007AFF] w-[72px] h-[72px] p-1  rounded-full">
         <View className="border border-white rounded-full w-full h-full  flex  flex-row  justify-center  items-center ">
           <Image
